8e3508379755e62bd1a3e8833f0bc5bbec7be52d
arvind11kumar <71036209+arvind11kumar@users.noreply.github.com>
2022-07-24 16:40:02 +0530

    Add files via upload

---
+++ Assinment4.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ol>
+        <li key="location1">Goa</li>
+        <li key="location2">Amsterdam</li>
+        <li key="location2">new York</li>
+        <li key="location1">Darjeeling</li>
+        <li key="location2">Tokyo</li>
+        <li key="location1">Lonavala</li>
+        
+        </ol>
+    </div>
+  );
+}
